72a8ab9a4c0154ef1c5459e5b8648384c269fd1e
Shamith <shamith301102@gmail.com>
2023-06-25 15:14:50 +0530

    Create U1_E1.java

---
+++ U1_E1.java
@@ -0,0 +1,20 @@
+// Question:
+// Write a Java program to display ‘Welcome to Java Programming!’ on the screen
+
+// Input Format
+// None
+
+// Constraints
+// None
+
+// Output Format
+// Single line string.
+
+// Sample Output 0
+// Welcome to Java Programming!
+
+public class U1_E1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
