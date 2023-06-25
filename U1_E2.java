acefee4683d0f666058b5d6147f73681a099941f
Shamith <shamith301102@gmail.com>
2023-06-25 15:42:29 +0530

    Update U1_E2.java

--- U1_E2.java
+++ U1_E2.java
@@ -1,3 +1,31 @@
+// Question:
+// Write a Java program to print an integer which is given by the user.
+
+// Note:
+// Use the below syntax to read integers from the user,
+
+// Scanner s=new Scanner(System.in);
+// int num=s.next();
+
+// Use s.next() --> To read-string
+// Use s.nextFloat() --> To read float value
+
+// Input Format
+//Single-line integer
+
+// Constraints
+// None
+
+// Output Format
+// Refer to sample test case.
+
+// Sample Input 0
+// 47
+
+// Sample Output 0
+// Given number is: 47
+
+
 import java.util.Scanner;
 
 public class U1_E2 {
