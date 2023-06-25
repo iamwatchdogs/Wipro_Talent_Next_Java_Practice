59e5f1c15ed08fef9eae8b4b177e263c556e02d6
Shamith <shamith301102@gmail.com>
2023-06-25 15:28:36 +0530

    Create U1_E2.java

---
+++ U1_E2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class U1_E2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println("Given number is: " + num);
+    }
+}
