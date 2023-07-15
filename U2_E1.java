0ceab8562a94de2056d4635283c9415aa05e3c6e
Shamith <shamith301102@gmail.com>
2023-07-15 23:51:42 +0530

    Create U2_E1.java

---
+++ U2_E1.java
@@ -0,0 +1,18 @@
+// Question:
+// Write a Java program illustrate command-line arguments.
+// Note:Complete this program in offline interpreter
+
+// Input Format:
+// java Filename String1 String2
+
+// Constraints:
+// None
+
+// Output Format:
+// String2:String1
+
+public class U2_E1 {
+    public static void main (String [] args){
+        System.out.println(args[1] + ":" + args[0]);
+    }
+}
