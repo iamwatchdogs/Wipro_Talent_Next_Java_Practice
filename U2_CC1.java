83aea6af51fdbb46bbafcab00a07a54ba2d3963a
Shamith <shamith301102@gmail.com>
2023-07-16 01:41:18 +0530

    Create U2_CC1.java

---
+++ U2_CC1.java
@@ -0,0 +1,27 @@
+// Question:
+// Write a Java program to display default value of all primitive data type of JAVA
+
+// Input Format:
+// None
+
+// Constraints:
+// None
+
+// Output Format:
+// Refer sample test case
+
+// Sample Output 0:
+// Integer:0
+// Float:0.0
+// Double:0.0
+// Boolean:false
+
+public class U2_CC1{
+    public static void main(String [] args){
+        int i = 0;
+        float f = 0;
+        double d = 0;
+        boolean b = false;
+        System.out.printf("Integer:%d\nFloat:%.1f\nDouble:%.1f\nBoolean:%b",i,f,d,b);
+    }
+}
